b0a890964275327be3fc9721161eb1a6cb1fa512
Mohamed Amine <mohamedaminebenhmida4@gmail.com>
2022-08-30 00:35:07 +0200

    value-issue

--- checkpoint.js
+++ checkpoint.js
@@ -34,27 +34,69 @@ function ready ()
         Button.addEventListener("click" ,removeCartItem)
     }
 }
+//Quantity Changes
+var quantityInputs = document.getElementsByClassName('cart-quantity');
+for (var i=0;i<quantityInputs.length; i++)
+{
+var input = quantityInputs[i];
+input.addEventListener("change",quantitychanged);
+}
+//Add to cart
+var addcart = document.getElementsByClassName('add-cart')
+for ( var i =0;i< addcart.length;i++)
+{
+    var button = addcart[i];
+ button.addEventListener("click" , addcartclicked)   ;
+}
+
+
+
+
+//Remove items from cart
 
 function removeCartItem(event){
     var buttonclicked = event.target;
     buttonclicked.parentElement.remove();
     Updatetotal ()
 }
+//Quantity changes
+function quantitychanged (event)
+{
+var input = event.target;
+if (isNaN (input.value) ||  (input.value <=0))
+{
+    input.value=1;
+}
+Updatetotal();
+}
+//dd to cart 
+function addcartclicked (event)
+{
+    var button = event.target;
+    var shopProducts= button.parentElement;
+    var title = shopProducts.getElementsByClassName("product-title")[0].innerText;
+    var price = shopProducts.getElementsByClassName("price")[0].innertext;
+    var productIMG = shopProducts.getElementsByClassName("product-img")[0].src;
+    console.log(title, price, productIMG);
+
+}
 
 //Update Total
 function Updatetotal (){
 var cartcontent = document.getElementsByClassName("cart-content")[0];
-var cartBoxes = cartcontent.getElementsByClassName("cart-box");
+var cartBoxes = cartcontent.getElementsByClassName("cart_box");
 var total=0;
 for (var i=0;i<cartBoxes.length; i++)
 {
     var cartBox =cartBoxes[i];
-    var priceElement = cartBox.getElementsByClassName("cart-price")[0];
+
+    var priceElement = cartBox.getElementsByClassName ("price")[0];
     var quantityElement= cartBox.getElementsByClassName("cart-quantity")[0];
-    var price =parseFloat(priceElement.innertext.replace("$",""));
+
     var quantity = quantityElement.value ;
     total=total + (price * quantity);
-    document.getElementsByClassName("total-price")[0].innertext="$" + total;
 }
+    document.getElementsByClassName("total-price")[0].innertext="$" + total;
+
 
 }

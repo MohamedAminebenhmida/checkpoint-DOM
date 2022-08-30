a4f0f841a61adbfe448b91cec86fa00e1290bdc7
Mohamed Amine <mohamedaminebenhmida4@gmail.com>
2022-08-30 11:11:07 +0200

    issue-fixed

--- checkpoint.js
+++ checkpoint.js
@@ -4,12 +4,12 @@ let Closecart = document.querySelector("#Close-cart");
 
 cartIcon.onclick = () => {
     cart.classList.add ("active");
-};
+}
 
 Closecart. onclick = ( )=> {
     cart.classList.remove("active");
 
-};
+}
 
 // cart working JS
 
@@ -35,7 +35,7 @@ function ready ()
     }
 }
 //Quantity Changes
-var quantityInputs = document.getElementsByClassName('cart-quantity');
+var quantityInputs = document.getElementsByClassName("cart-quantity");
 for (var i=0;i<quantityInputs.length; i++)
 {
 var input = quantityInputs[i];
@@ -62,14 +62,17 @@ function removeCartItem(event){
 //Quantity changes
 function quantitychanged (event)
 {
-var input = event.target;
-if (isNaN (input.value) ||  (input.value <=0))
+var input =event && event.target;
+if (isNaN (input && input.value) ||  (input && input.value <=0))
 {
-    input.value=1;
+   if(input){
+    input.value=1
+   }
 }
 Updatetotal();
 }
-//dd to cart 
+document.querySelector('.cart-quantity').addEventListener('change',quantitychanged)
+//Add to cart 
 function addcartclicked (event)
 {
     var button = event.target;
@@ -81,22 +84,24 @@ function addcartclicked (event)
 
 }
 
+
 //Update Total
 function Updatetotal (){
 var cartcontent = document.getElementsByClassName("cart-content")[0];
 var cartBoxes = cartcontent.getElementsByClassName("cart_box");
+
 var total=0;
 for (var i=0;i<cartBoxes.length; i++)
 {
     var cartBox =cartBoxes[i];
 
-    var priceElement = cartBox.getElementsByClassName ("price")[0];
+    var price = cartBox.getElementsByClassName ("Price")[0];
     var quantityElement= cartBox.getElementsByClassName("cart-quantity")[0];
 
     var quantity = quantityElement.value ;
-    total=total + (price * quantity);
+    total=total + (price.innerText * quantity);
 }
-    document.getElementsByClassName("total-price")[0].innertext="$" + total;
+    document.getElementsByClassName("totalPrice")[0].innerText=total;
 
 
 }
